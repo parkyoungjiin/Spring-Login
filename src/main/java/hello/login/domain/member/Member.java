a5cb91117184a5f1ac252cbd36998e92be86e96c
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-15 15:48:45 +0900

    feat: 회원가입 로직, 테스트용 member 추가

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,17 @@
+package hello.login.domain.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Memeber {
+    private Long id;
+    @NotBlank
+    private String loginId; //로그안 ID
+    @NotEmpty
+    private String name; //사용자 이름
+    @NotEmpty
+    private String password;
+}
